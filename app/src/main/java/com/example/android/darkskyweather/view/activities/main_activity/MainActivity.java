f444c8f328326db6b054b5a26aa39937464a9f7a
rgreen88 <rynelgreen@gmail.com>
2018-03-08 19:04:04 -0500

    fixed crashing removing Glide dependency

--- app/src/main/java/com/example/android/darkskyweather/view/activities/main_activity/MainActivity.java
+++ app/src/main/java/com/example/android/darkskyweather/view/activities/main_activity/MainActivity.java
@@ -5,31 +5,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.android.darkskyweather.R;
 import com.example.android.darkskyweather.model.DailyDatum;
+import com.example.android.darkskyweather.model.WeatherInformation;
+import com.example.android.darkskyweather.view.injection.main_activity.DaggerMainComponent;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements MainContract{
+public class MainActivity extends AppCompatActivity implements MainContract.View{ // you need to add .View to this
 
     private RecyclerView weatherView;
-
-
+    MainPresenter presenter;
     RecyclerView.ItemAnimator itemAnimator;
     private List<DailyDatum> weatherList = new ArrayList<>();
     private WeatherAdapter weatherAdapter;
-
-    TextView city;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        city = findViewById(R.id.tv_city);
+        setupDagger();
+
+        ImageView icon = findViewById(R.id.iv_condition);
+        TextView city = findViewById(R.id.tv_city);
         weatherView = findViewById(R.id.rv_weather);
 
         //RecyclerView LinearLayoutManager
@@ -45,5 +48,25 @@ public class MainActivity extends AppCompatActivity implements MainContract{
         //setting adapter
         weatherAdapter = new WeatherAdapter(weatherList);
         weatherView.setAdapter(weatherAdapter);
+    }
+
+    @Override
+    public void showError(String error) {
+
+    }
+
+    @Override
+    public void setupDagger() {
+        DaggerMainComponent.create().inject(this);
+    }
+
+    @Override
+    public void setupAdapter(WeatherInformation information) {
+
+    }
+
+    @Override
+    public void showDetailedInformation() {
+
     }
 }
--- app/src/main/java/com/example/android/darkskyweather/view/activities/main_activity/MainPresenter.java
+++ app/src/main/java/com/example/android/darkskyweather/view/activities/main_activity/MainPresenter.java
@@ -4,5 +4,22 @@ package com.example.android.darkskyweather.view.activities.main_activity;
  * Created by rynel on 3/8/2018.
  */
 
-public class MainPresenter {
+public class MainPresenter implements MainContract.Presenter{
+
+    MainContract.View view;
+
+    @Override
+    public void addView(MainContract.View view) {
+        this.view = view;
+    }
+
+    @Override
+    public void removeView() {
+        this.view = null;
+    }
+
+    @Override
+    public void getWeatherInformation(double lat, double lng) {
+        //retrofit call
+    }
 }
